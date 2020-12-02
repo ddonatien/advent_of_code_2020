a383ca401c2ae9b2c02e6ebbd80d694ba3cb8251
Donatien Delehelle <ddelehel@gmail.com>
2020-12-02 14:32:07 +0100

    Day 1 part 1

---
+++ day1/src/main.rs
@@ -0,0 +1,48 @@
+extern crate regex;
+
+use std::fs::File;
+use regex::Regex;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main(){
+    let re = Regex::new(r"(\d+)-(\d+) ([a-z]): ([a-z]+)").unwrap();
+    let mut count: u16 = 0;
+    if let Ok(lines) = read_lines("./data.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                let cap = re.captures(&ip).unwrap();
+                let min: u8 = cap[1].parse::<u8>().unwrap();
+                let max: u8 = cap[2].parse::<u8>().unwrap();
+                let l: char = cap[3].chars().next().unwrap();
+                if check_pwd(min, max, l, &cap[4]) {
+                    count +=1;
+                }
+            }
+        }
+    }
+    println!("{} valid passwords", count);
+}
+
+fn check_pwd(min: u8, max: u8, l: char, pwd: &str) -> bool {
+    let mut count: u8 = 0;
+    let res: bool;
+    for c in pwd.chars() {
+        if c == l {
+            count +=1;
+        }
+    }
+    if (count >= min) & (count <= max) {
+        res = true;
+    }
+    else {
+        res = false;
+    }
+    return res;
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
